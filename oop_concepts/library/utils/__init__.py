62ea47647d359df33e11060aab5ad5e1d7c01408
dev:grump <jccf541@gmail.com>
2024-12-28 13:05:23 -0600

    added permanency with file i/o and created __init__ file for utils module, updated main to highlight new functionalities.

---
+++ oop_concepts/library/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utils Package
+=============
+Provides utility functions for file management and ID generation.
+"""
+
+from .file_manager import save_data, load_data, serialize_library, deserialize_library
+from .id_generator import generate_id
